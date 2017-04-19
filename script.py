eed2a4d3be4857cdcb3e2d3419ab789548edf428
leonrave <alex_leon16@hotmail.com>
2017-04-19 17:20:30 -0500

    Scirpt para mesos

---
+++ script.py
@@ -0,0 +1,15 @@
+a=1
+
+while [ $a -le 1000000 ]
+do
+        echo $a
+        echo "Escribiendo... $a">>archivo.txt
+        (( a++ ))
+
+done
+
+fin=`date +%s`
+let total=$fin-$inicio
+
+echo "ha tardado: $total segundos"
+
